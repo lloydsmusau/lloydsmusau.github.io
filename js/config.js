94212cdebe5b5cb94f27040b38abc4c8cd5fde4c
lloydsmusau <lloydsmusau02@gmail.com>
2026-08-25 17:07:52 +0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,1 +1,18 @@
+const SITE_CONFIG = {
 
+    CMS_API:
+        "https://script.google.com/macros/s/AKfycbws9z9PxztslMascaAClm8sgsOaLC4ZIpqSePGKnXA2p7DqofBcbPAYyUcHQCNNWWql/exec",
+
+    CALENDAR_BOOKING_URL:
+        "https://calendar.app.google/HBfmMpEvpawdYgMt8",
+
+    SITE_NAME:
+        "Lloyds Musau",
+
+    SITE_TITLE:
+        "IT Administrator | IT Operations & Systems",
+
+    LOCATION:
+        "Nairobi, Kenya"
+
+};
